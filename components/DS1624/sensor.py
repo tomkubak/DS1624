248afdf7e56dabe519b4b1449cdf09fb23438234
tomkubak <134051136+tomkubak@users.noreply.github.com>
2024-03-05 13:12:14 +0100

    Update sensor.py

--- components/DS1624/sensor.py
+++ components/DS1624/sensor.py
@@ -1,22 +1,37 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
-from esphome.components import sensor, i2c
-from esphome.const import CONF_ADDRESS, UNIT_EMPTY, ICON_EMPTY
+from esphome.components import sensor
+from esphome.const import (
+    CONF_ID,
+    DEVICE_CLASS_TEMPERATURE,
+    UNIT_EMPTY,
+    ICON_EMPTY,
+    UNIT_CELSIUS,
+)
 
-DEPENDENCIES = ['i2c']
+CONF_DS1624_ID = "ds1624_id"
+CONF_UPDATE_INTERVAL = "update_interval"
+CONF_ADDRESS = "address"
 
-ds1624_ns = cg.esphome_ns.namespace('sensor').namespace('ds1624')
+ds1624_ns = cg.esphome_ns.namespace("ds1624")
+DS1624Temperature = ds1624_ns.class_("DS1624Temperature", sensor.Sensor, cg.Component)
 
-DS1624Sensor = ds1624_ns.class_('DS1624Sensor', sensor.Sensor, cg.Component)
+CONFIG_SCHEMA = cv.Schema(
+    {
+        cv.GenerateID(CONF_DS1624_ID): cv.declare_id(DS1624Temperature),
+        cv.Optional(CONF_UPDATE_INTERVAL, default="15s"): cv.positive_time_period_milliseconds,
+        cv.Optional(CONF_ADDRESS, default=0x48): cv.hex_uint8_t,
+    }
+).extend(cv.COMPONENT_SCHEMA)
 
-CONFIG_SCHEMA = sensor.sensor_schema(unit_of_measurement=UNIT_EMPTY, icon=ICON_EMPTY).extend({
-    cv.GenerateID(): cv.declare_id(DS1624Sensor),
-    cv.Required(CONF_ADDRESS): cv.i2c_address,
-}).extend(i2c.i2c_device_schema(CONF_ADDRESS))
 
-def to_code(config):
-    var = cg.new_Pvariable(config[CONF_ID])
-    yield cg.register_component(var, config)
+async def to_code(config):
+    var = cg.new_Pvariable(config[CONF_DS1624_ID])
+    await cg.register_component(var, config)
+    await sensor.register_sensor(var, config)
+
+    cg.add(var.set_update_interval(config[CONF_UPDATE_INTERVAL]))
     cg.add(var.set_address(config[CONF_ADDRESS]))
-    yield sensor.register_sensor(var, config)
-    yield i2c.register_i2c_device(var, config)
+    cg.add(var.set_device_class(DEVICE_CLASS_TEMPERATURE))
+    cg.add(var.set_unit_of_measurement(UNIT_CELSIUS))
+    cg.add(var.set_icon(ICON_EMPTY))
