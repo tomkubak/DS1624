40083e078f09eace46b379315dff0d35043da9af
tomkubak <134051136+tomkubak@users.noreply.github.com>
2024-03-05 12:10:45 +0100

    Update DS1624.cpp

--- components/DS1624/DS1624.cpp
+++ components/DS1624/DS1624.cpp
@@ -1,43 +1,28 @@
-#ifndef DS1624_H
-#define DS1624_H
+#include "DS1624.h"
 
-#include "esphome.h"
+void DS1624::setup() {
+  Wire.begin();
+}
 
-class MyDS1624Sensor : public PollingComponent, public Sensor {
- public:
-  MyDS1624Sensor() : PollingComponent(15000) {}
+float DS1624::readTemperature() {
+  Wire.beginTransmission(0x48);
+  Wire.write(0xAA); // Command to start temperature conversion
+  Wire.endTransmission();
+  delay(20); // Wait for conversion to finish
 
-  void setup() override {
-    Wire.begin();
-  }
-
-  void update() override {
-    Wire.beginTransmission(0x48);
-    Wire.write(0xAA); // Command to start temperature conversion
-    Wire.endTransmission();
-    delay(20); // Wait for conversion to finish
-
-    Wire.beginTransmission(0x48);
-    Wire.write(0xAA); // Command to read temperature
-    Wire.endTransmission();
-    
-    Wire.requestFrom(0x48, 2); // Request 2 bytes of temperature data
-    if (Wire.available() >= 2) {
-      uint8_t msb = Wire.read();
-      uint8_t lsb = Wire.read();
-      float temperature = convertTemperature(msb, lsb);
-      publish_state(temperature);
-    }
-  }
-
-  float convertTemperature(uint8_t msb, uint8_t lsb) {
+  Wire.beginTransmission(0x48);
+  Wire.write(0xAA); // Command to read temperature
+  Wire.endTransmission();
+  
+  Wire.requestFrom(0x48, 2); // Request 2 bytes of temperature data
+  if (Wire.available() >= 2) {
+    uint8_t msb = Wire.read();
+    uint8_t lsb = Wire.read();
     int16_t rawTemperature = (msb << 8) | lsb;
     if (rawTemperature & 0x8000) {
       rawTemperature = -(~rawTemperature + 1);
     }
     return rawTemperature * 0.0625; // Each bit represents 0.0625 degrees Celsius
   }
-};
-
-#endif
-
+  return -1000; // Return an invalid value if reading failed
+}
