0a432f20606b7b087ea31b7827139775f04532e6
tomkubak <134051136+tomkubak@users.noreply.github.com>
2024-03-05 13:08:34 +0100

    Update sensor.py

--- components/DS1624/sensor.py
+++ components/DS1624/sensor.py
@@ -1,8 +1,7 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
-from esphome.components import sensor
-from esphome.const import CONF_UPDATE_INTERVAL
-from . import DS1624
+from esphome.components import sensor, i2c
+from esphome.const import CONF_ADDRESS
 
 DEPENDENCIES = ['i2c']
 
@@ -10,15 +9,13 @@ ds1624_ns = cg.esphome_ns.namespace('sensor').namespace('ds1624')
 
 DS1624Sensor = ds1624_ns.class_('DS1624Sensor', sensor.Sensor, cg.Component)
 
-CONFIG_SCHEMA = sensor.sensor_schema().extend({
+CONFIG_SCHEMA = sensor.sensor_schema('Temperature', cv.float_).extend({
     cv.GenerateID(): cv.declare_id(DS1624Sensor),
-}).extend(cv.polling_component_schema()).extend({
-    cv.GenerateID(): cv.declare_id(DS1624.DS1624),
-}).extend(cv.polling_component_schema('60s')).extend(i2c.i2c_device_schema(CONF_I2C_ADDR))
+}).extend(i2c.i2c_device_schema(CONF_ADDRESS))
 
 def to_code(config):
-    var = cg.new_Pvariable(config[CONF_DS1624_ID])
+    var = cg.new_Pvariable(config[CONF_ID])
     yield cg.register_component(var, config)
-    cg.add(var.set_i2c_component(config[CONF_I2C_ID]))
+    cg.add(var.set_address(config[CONF_ADDRESS]))
     yield sensor.register_sensor(var, config)
     yield i2c.register_i2c_device(var, config)
