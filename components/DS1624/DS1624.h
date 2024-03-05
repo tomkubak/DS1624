b7e385788cda329b2e33aa31f8603364b936ea67
tomkubak <134051136+tomkubak@users.noreply.github.com>
2024-03-05 12:49:57 +0100

    Update DS1624.h

--- components/DS1624/DS1624.h
+++ components/DS1624/DS1624.h
@@ -1,13 +1,20 @@
-#ifndef DS1624_H
-#define DS1624_H
+#pragma once
 
-#include <Wire.h>
-#include <esphome.h>
+#include "esphome/core/component.h"
+#include "esphome/core/hal.h"
+#include "esphome/components/sensor/sensor.h"
 
-class DS1624 {
-public:
-  void setup();
+namespace esphome {
+namespace ds1624 {
+
+class DS1624 : public sensor::Sensor, public Component {
+ public:
+  void setup() override;
+  void update() override;
+  
+ protected:
   float readTemperature();
 };
 
-#endif
+}  // namespace ds1624
+}  // namespace esphome
