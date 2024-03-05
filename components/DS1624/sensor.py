3656481be7125119a7796dce5a5a582ee6312d38
tomkubak <134051136+tomkubak@users.noreply.github.com>
2024-03-05 13:09:47 +0100

    Update sensor.py

--- components/DS1624/sensor.py
+++ components/DS1624/sensor.py
@@ -1,7 +1,7 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome.components import sensor, i2c
-from esphome.const import CONF_ADDRESS
+from esphome.const import CONF_ADDRESS, UNIT_EMPTY, ICON_EMPTY
 
 DEPENDENCIES = ['i2c']
 
@@ -9,8 +9,9 @@ ds1624_ns = cg.esphome_ns.namespace('sensor').namespace('ds1624')
 
 DS1624Sensor = ds1624_ns.class_('DS1624Sensor', sensor.Sensor, cg.Component)
 
-CONFIG_SCHEMA = sensor.sensor_schema('Temperature', cv.float_).extend({
+CONFIG_SCHEMA = sensor.sensor_schema(unit_of_measurement=UNIT_EMPTY, icon=ICON_EMPTY).extend({
     cv.GenerateID(): cv.declare_id(DS1624Sensor),
+    cv.Required(CONF_ADDRESS): cv.i2c_address,
 }).extend(i2c.i2c_device_schema(CONF_ADDRESS))
 
 def to_code(config):
