ca7e5069032ae9b08115b7cca1cb03398ba3fd62
tomkubak <134051136+tomkubak@users.noreply.github.com>
2024-03-05 12:28:31 +0100

    Update sensor.py

--- components/DS1624/sensor.py
+++ components/DS1624/sensor.py
@@ -1,18 +1,18 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome.components import i2c, sensor
-from esphome.const import CONF_ID, UNIT_EMPTY, ICON_EMPTY
+from esphome.const import CONF_ID, ICON_EMPTY, UNIT_EMPTY
 
 DEPENDENCIES = ['i2c']
 
-CONF_DS1624_ADDR = 0x48
+CONF_I2C_ADDR = 0x48  # Adresa čipu DS1624 na I2C sběrnici
 
-ds1624_ns = cg.esphome_ns.namespace('ds1624')
+ds1624_ns = cg.esphome_ns.namespace('sensor').namespace('ds1624')
 DS1624Sensor = ds1624_ns.class_('DS1624Sensor', cg.PollingComponent, i2c.I2CDevice)
 
 CONFIG_SCHEMA = sensor.sensor_schema(UNIT_EMPTY, ICON_EMPTY, 1).extend({
     cv.GenerateID(): cv.declare_id(DS1624Sensor),
-}).extend(cv.polling_component_schema('60s')).extend(i2c.i2c_device_schema(CONF_DS1624_ADDR))
+}).extend(cv.polling_component_schema('60s')).extend(i2c.i2c_device_schema(CONF_I2C_ADDR))
 
 def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
